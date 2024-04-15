c0a250e4761dc2aec93baf4c71bd2aaf371b8ab3
CalleAlexanderson <calle.alexanderson@telia.com>
2024-04-16 00:03:25 +0200

    konvertions sidan innehåll klart, inte style:at

--- src/app/unit-convertion/unit-convertion.component.ts
+++ src/app/unit-convertion/unit-convertion.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-unit-convertion',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './unit-convertion.component.html',
   styleUrl: './unit-convertion.component.scss'
 })
 export class UnitConvertionComponent {
 
+  gMass: number = 0;
+  cMass: number = 0;
+  sMoney: number = 0;
+  cMoney: number = 0;
+  massConversionG():void{
+    this.cMass = this.gMass*2;
+  }
+
+  massConversionC():void{
+    this.gMass = this.cMass/2;
+  }
+
+  moneyConversionSEK():void{
+    this.cMoney = this.sMoney*0.67;
+  }
+
+  moneyConversionCNY():void{
+    this.sMoney = this.cMoney*1.5;
+  }
 }
