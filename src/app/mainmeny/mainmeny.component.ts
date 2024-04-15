9b8a76723750e0b360a38032d2637cbecafabcd1
CalleAlexanderson <calle.alexanderson@telia.com>
2024-04-15 18:27:09 +0200

    meny funkar

--- src/app/mainmeny/mainmeny.component.ts
+++ src/app/mainmeny/mainmeny.component.ts
@@ -9,5 +9,19 @@ import { RouterLink } from '@angular/router';
   styleUrl: './mainmeny.component.scss'
 })
 export class MainmenyComponent {
+  menu: HTMLDivElement;
+  constructor (){
+    console.log("works");
+    // funkar inte för (jag tror) DOM har inte laddat in när constructorn körs, men måste ändå ge menu något värde
+    this.menu = document.getElementById('mobile_menu') as HTMLDivElement;
+  }
 
+  openMenu():void{
+    this.menu = document.getElementById('mobile_menu') as HTMLDivElement;
+    this.menu.style.visibility = "visible";
+  }
+  closeMenu():void{
+    this.menu = document.getElementById('mobile_menu') as HTMLDivElement;
+    this.menu.style.visibility = "hidden";
+  }
 }
